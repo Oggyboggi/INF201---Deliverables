fc7afe2068516b1350de0f16b9f3b2ae9ed86e8f
Oscar Wiersdalen Thunold <79252382+Oggyboggi@users.noreply.github.com>
2025-11-10 18:08:48 +0100

    Add files via upload

--- Deliverable3/complex.py
+++ Deliverable3/complex.py
@@ -1,5 +1,42 @@
+
 import math
+
+class Complex:
+    def __init__(self, re, im=0):
+        self.re = re
+        self.im = im
         
+    def __repr__(self):
+        return f'Complex({self.re}, {self.im})'
+    
+    def __add__(self, rhs):
+        return Complex(self.re + rhs.real, self.im + rhs.imag)
+    
+    def __sub__(self, rhs):
+        return Complex(self.re - rhs.real, self.im - rhs.imag)
+    
+    def __mul__(self, rhs):
+        if isinstance(rhs, complex):
+            re = self.re * rhs.real - self.im * rhs.imag
+            im = self.re * rhs.imag + self.im * rhs.real
+            return Complex(re, im)
+        return Complex(self.re * rhs, self.im * rhs)
+    
+    def __rmul__(self, lhs):
+        return self * lhs
+    
+    def __eq__(self, rhs):
+        if isinstance(rhs, Complex):
+            return self.re == rhs.re and self.im == rhs.im
+    
+    def __str__(self):
+        return f'({self.re} + {self.im}i)' if self.im >= 0 else f'({self.re} - {-self.im}i)'
+
+
+
+
+
+
 """
 class Vector():
 
